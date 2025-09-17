fc8405d0b5bd4ba44e80599f48dccbf900bb8fc0
ikeepo <ikeepo@proton.me>
2025-09-16 23:10:40 -0400

    12:09

--- src/components/HelpWidget.tsx
+++ src/components/HelpWidget.tsx
@@ -1,3 +1,11 @@
+import { useState } from "react"
+
 export const HelpWidget = () => {
-    return <div className="fixed bottom-10 right-10">Help</div>
+    const [isDisplayed, setIsDisplayed] = useState(false);
+
+    return isDisplayed ? <div className="fixed bottom-10 right-10 h-96 w-52  bg-white text-blue p-8 ">
+        <button onClick={() => setIsDisplayed(false)}>X</button>
+    </div> : (<button onClick={() => setIsDisplayed(true)}
+        className="fixed bottom-10 right-10 bg-blue-400 text-white p-2 px-3 hover:bg-blue-500"
+    >Speak to Support Team(Real Person)</button>)
 }
